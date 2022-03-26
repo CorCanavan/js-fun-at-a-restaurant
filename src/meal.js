ef9253e2558b6d90f6572e337f2338962c9b0ab2
“Corinne <corinne.canavan@gmail.com>
2022-03-25 20:57:44 -0600

    Add complete meal functionality and unskip meal tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,43 @@
-function nameMenuItem() {
-
+function nameMenuItem(foodItem) {
+  return `Delicious ${foodItem}`;
 }
 
+function createMenuItem(foodName, foodPrice, foodType) {
+  return {
+    name: foodName,
+    price: foodPrice,
+    type: foodType
+  };
+}
+
+function addIngredients(ingredientItem, ingredients) {
+  if(!ingredients.includes(ingredientItem)) {
+    return ingredients.push(ingredientItem);
+  }
+}
+
+function formatPrice(price) {
+  return `$${price}`;
+}
+
+function decreasePrice(price) {
+  var tenPercent = (price * .10);
+  return price - tenPercent;
+}
+
+function createRecipe(recipeTitle, recipeIngredients, recipeType) {
+  return {
+    title: recipeTitle,
+    ingredients: recipeIngredients,
+    type: recipeType
+  };
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
