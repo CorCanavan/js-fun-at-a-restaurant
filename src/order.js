ea4f2254b7e33a97dc090f3e07ff4153653bd967
“Corinne <corinne.canavan@gmail.com>
2022-03-26 21:24:45 -0600

    Add complete order functionality and unskip order tests

--- src/order.js
+++ src/order.js
@@ -1,7 +1,37 @@
+function takeOrder(foodOrder, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+    return deliveryOrders.push(foodOrder);
+  }
+}
+
+function refundOrder(orderNumber, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === orderNumber) {
+      deliveryOrders.splice([i], 1);
+    }
+  }
+}
+
+function listItems(deliveryOrders) {
+  var itemList = [];
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    itemList.push(deliveryOrders[i].item);
+  }
+  return itemList.join(', ');
+}
+
+function searchOrder(deliveryOrders, itemName) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item === itemName) {
+      return true
+    }
+  }
+  return false
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
